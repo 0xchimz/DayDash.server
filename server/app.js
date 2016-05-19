2df54522f56d680086636560b44071d029c925e6
ChinCluBi <chincub@gmail.com>
2016-05-20 01:57:28 +0700

    Create room automatic and join room

--- server/app.js
+++ server/app.js
@@ -1,50 +1,38 @@
 var Server = require('socket.io')
 var io = new Server()
 
-var STATUS = {
-	IDLE: -1,
-	WAIT1: 0,
-	WAIT2: 1,
-	START: 2,
-	END: 3
-}
+var Room = require('./room')
 
-var ROLE = {
-	FINDER: 1,
-	ESCAPER: 2
-}
+var MAX_PLAYER = 2
 
+var rooms = {}
 var users = []
 
-io.on('connection', function (socket) {
-	console.log('New user connected.')
-	let status = STATUS.IDLE
+var cRoom
+
+io.sockets.on('connection', function (socket) {
+	console.log(socket.id + ' connected.')
 
 	socket.on('JOIN_ROOM', function() {
-		if(status !== STATUS.IDLE && status !== STATUS.WAIT1){
-			console.log('Room is not empty.')
-			socket.emit('USER_DISCONNECTED', {msg: "Room is not empty."})
-			socket.disconnect()
-			return;
+		// Create New Room if the room is not empty or undefined.
+		if(!cRoom){
+			console.log("cRoom is undefined.")
+			cRoom = new Room(MAX_PLAYER)
+		} else if(!cRoom.isEmpty()){
+			console.log("Room no." + cRoom.getRoomNo() + " is not empty.")
+			cRoom = new Room(MAX_PLAYER)
 		}
-		console.log('Join Room')
 
-		if(status === STATUS.IDLE){
-			let isFind = (Math.random() > 0.5)
-			users[0] = socket
-			users[0].role = isFind ? ROLE.FINDER : ROLE.ESCAPER
-			status = STATUS.WAIT1
-		} else {
-			users[1] = socket
-			users[1].role = (users[0].role === ROLE.FINDER) ? ROLE.ESCAPER : ROLE.FINDER
-			status = STATUS.WAIT2
+		socket.join(cRoom.getRoomNo())
+		socket.room = cRoom
+		cRoom.joinRoom()
+		console.log(socket.id + ' join room no.' + cRoom.getRoomNo())
+
+		socket.emit('JOIN_RESPONSE', {roomInfo: cRoom.getRoomInfo()})
+
+		if(cRoom.currentPlayer === 2){
+			io.sockets.in(cRoom.getRoomNo()).emit('ROOM', {})
 		}
-		let role = (users[status].role === ROLE.FINDER) ? 'finder' : 'escaper'
-		socket.emit('USER_ROLE', {role: role})
-	})
-
-	socket.on('disconnect', function() {
-		console.log('User disconnected.')
 	})
 })
 
---
+++ server/room.js
@@ -0,0 +1,67 @@
+module.exports = Room
+
+var STATUS = {
+	WAITING: 0,
+	START: 1,
+	END: 2
+}
+
+var ROLE = {
+	FINDER: 1,
+	ESCAPER: 2
+}
+
+var currentRoom = 0
+
+function Room(maxPlayer){
+	this.roomNo = ++currentRoom
+	this.maxPlayer = maxPlayer
+	this.currentPlayer = 0
+	this.status = STATUS.WAITING
+	this.level = 1
+
+	this.getRoomNo = function() {
+		return this.roomNo
+	}
+
+	this.getLevel = function() {
+		return this.level
+	}
+
+	this.nextLevel = function() {
+		this.level++
+	}
+
+	this.getRoomInfo = function() {
+		let info = {
+			no: this.roomNo,
+			level: this.level,
+			status: this.status
+		}
+		return info
+	}
+
+	this.joinRoom = function(){
+		if(this.isEmpty()){
+			this.currentPlayer++
+
+			// Random role for player
+			// if(room.status === STATUS.IDLE){
+			// 	let role = (Math.random() > 0.5)
+			// 	users[0] = socket
+			// 	users[0].role = isFind ? ROLE.FINDER : ROLE.ESCAPER
+			// } else {
+			// 	users[1] = socket
+			// 	users[1].role = (users[0].role === ROLE.FINDER) ? ROLE.ESCAPER : ROLE.FINDER
+			// }
+
+			// let role = (users[status].role === ROLE.FINDER) ? 'finder' : 'escaper'
+		} else {
+			return false
+		}
+	}
+
+	this.isEmpty = function () {
+		return (this.maxPlayer > this.currentPlayer)
+	}
+}
