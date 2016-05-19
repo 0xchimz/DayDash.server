1b33497f2db2cbc296937541a32bf6c0087cf323
ChinCluBi <chincub@gmail.com>
2016-05-19 13:54:03 +0700

    Joining room

--- server/app.js
+++ server/app.js
@@ -1,8 +1,31 @@
 var Server = require('socket.io')
 var io = new Server()
 
+var STATUS = {
+	IDLE: -1,
+	WAIT1: 0,
+	WAIT2: 1,
+	START: 2,
+	END: 3
+}
+
 io.on('connection', function (socket) {
 	console.log('New user connected.')
+	let status = STATUS.IDLE
+
+	socket.on('JOIN_ROOM', function() {
+		if(status !== STATUS.IDLE || status !== STATUS.WAIT1){
+			console.log('Room is not empty.')
+			socket.emit('USER_DISCONNECTED', {msg: "Room is not empty."})
+			socket.disconnect()
+			return;
+		}
+		console.log('Join Room')
+	})
+
+	socket.on('disconnect', function() {
+		console.log('User disconnected.')
+	})
 })
 
 io.listen(8000)
