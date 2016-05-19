f2e5c55a5eb36b93765bcae34702a86f0b1b148e
ChinCluBi <chincub@gmail.com>
2016-05-19 14:34:35 +0700

    Set role to client

--- server/app.js
+++ server/app.js
@@ -9,18 +9,38 @@ var STATUS = {
 	END: 3
 }
 
+var ROLE = {
+	FINDER: 1,
+	ESCAPER: 2
+}
+
+var users = []
+
 io.on('connection', function (socket) {
 	console.log('New user connected.')
 	let status = STATUS.IDLE
 
 	socket.on('JOIN_ROOM', function() {
-		if(status !== STATUS.IDLE || status !== STATUS.WAIT1){
+		if(status !== STATUS.IDLE && status !== STATUS.WAIT1){
 			console.log('Room is not empty.')
 			socket.emit('USER_DISCONNECTED', {msg: "Room is not empty."})
 			socket.disconnect()
 			return;
 		}
 		console.log('Join Room')
+
+		if(status === STATUS.IDLE){
+			let isFind = (Math.random() > 0.5)
+			users[0] = socket
+			users[0].role = isFind ? ROLE.FINDER : ROLE.ESCAPER
+			status = STATUS.WAIT1
+		} else {
+			users[1] = socket
+			users[1].role = (users[0].role === ROLE.FINDER) ? ROLE.ESCAPER : ROLE.FINDER
+			status = STATUS.WAIT2
+		}
+		let role = (users[status].role === ROLE.FINDER) ? 'finder' : 'escaper'
+		socket.emit('USER_ROLE', {role: role})
 	})
 
 	socket.on('disconnect', function() {
