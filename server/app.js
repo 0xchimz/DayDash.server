94f55979c07e53ead44cd491730b26c3edeae1a7
ChinCluBi <chincub@gmail.com>
2016-05-26 01:54:17 +0700

    Edit server flow to join game

--- server/app.js
+++ server/app.js
@@ -3,12 +3,18 @@ var io = new Server()
 
 var Room = require('./room')
 
-var MAX_PLAYER = 1
+var MAX_PLAYER = 2
 
 var rooms = {}
 var users = {}
 
 var cRoom
+
+var GAME_STATUS = {
+	LOADING: 1,
+	READY: 2,
+	PLAYING: 3
+}
 
 io.sockets.on('connection', function (client) {
 	console.log(client.id + ' connected.')
@@ -25,18 +31,36 @@ io.sockets.on('connection', function (client) {
 		client.room = cRoom
 		client.join(cRoom.getRoomNo())
 		client.item = cRoom.generateItem()
+		client.status = GAME_STATUS.LOADING
 		cRoom.joinRoom()
 		console.log(client.id + ' join room no.' + cRoom.getRoomNo())
 
 		client.emit('JOIN_RESPONSE', {roomInfo: cRoom.getRoomInfo()})
 		if(cRoom.currentPlayer === MAX_PLAYER){
 			console.log('Room no.' + cRoom.getRoomNo() + ' game is starting.')
-			let members = io.sockets.in(cRoom.getRoomNo()).adapter.rooms[cRoom.getRoomNo()].sockets
-			for(let member in members){
-				let itemList = io.sockets.connected[member].item
-				io.sockets.connected[member].emit('GENERATE_ITEM', {data: itemList})
-			}
-			io.sockets.in(cRoom.getRoomNo()).emit('START_GAME')
+			setTimeout(function () {
+				io.sockets.in(cRoom.getRoomNo()).emit('START_GAME')
+			}, 5000);
+		}
+	})
+
+	client.on('GET_ITEM', function() {
+		let members = io.sockets.in(cRoom.getRoomNo()).adapter.rooms[cRoom.getRoomNo()].sockets
+		for(let member in members){
+			let itemList = io.sockets.connected[member].item
+			io.sockets.connected[member].emit('GENERATE_ITEM', {data: itemList})
+		}
+	})
+
+	client.on('GAME_STATUS_READY', function() {
+		client.status = GAME_STATUS.READY
+		let play = true
+		let members = io.sockets.in(cRoom.getRoomNo()).adapter.rooms[cRoom.getRoomNo()].sockets
+		for(let member in members){
+			play = (play && (io.sockets.connected[member].status === GAME_STATUS.READY))
+		}
+		if(play){
+			io.sockets.in(cRoom.getRoomNo()).emit('PLAY_GAME')
 		}
 	})
 
